2a6095e6d0041857f1377a641fc8190f1a0688f2
loyalty-thameem <loyalty.thameem@gmail.com>
2022-09-28 10:39:27 +0530

    'added_images'

---
+++ src/screens/LocalData/HomeData.js
@@ -0,0 +1,28 @@
+export default [
+    {
+        id: 1,
+        title: "ORDER PARTS",
+        image: require('../../assets/images/system.png')
+    },
+    {
+        id: 2,
+        title: "MY FLEET",
+        image: require('../../assets/images/bulldozer.png')
+    },
+    {
+        id: 3,
+        title: "MY PLAN",
+        image: require('../../assets/images/calendar-interface-symbol-tool.png')
+    },
+    {
+        id: 4,
+        title: "ASSISTANT",
+        image: require('../../assets/images/help.png')
+    },
+    {
+        id: 5,
+        title: "MY QUOTE",
+        notification:' 2 ',
+        image: require('../../assets/images/price.png')
+    },
+]
